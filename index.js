c5bd7f2789291369c2e7e659b899eeeec7297f84
Atis <atis@aol.es>
2015-07-28 23:18:38 +0300

    isChannel()

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 
+var YateChannel = require('./lib/YateChannel').Channel;
+
+//
+// determine if given value is a channel instance
+//
+exports.isChannel = function (channel) {
+    return (channel instanceof YateChannel);
+}
+
+//
+// Yate external control module
+//
 exports.YateExt = require('./lib/Yate');
+
+//
+// FreeSWITCH external control module
+//
 //exports.FSExt = require('./lib/FreeSWITCH');
