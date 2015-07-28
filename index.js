47e276037c02bb75b357ccb97ac150a17bc50e42
Atis <atis@aol.es>
2015-07-28 23:34:25 +0300

    isIVR()

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
-var YateChannel = require('./lib/YateChannel').Channel;
+var YateChannel = require('./lib/YateChannel');
 
 //
 // determine if given value is a channel instance
 //
 exports.isChannel = function (channel) {
-    return (channel instanceof YateChannel);
+    return (channel instanceof YateChannel.Channel);
+}
+
+//
+// determine if given value is an IVR channel instance
+//
+exports.isIVR = function (channel) {
+    return (channel instanceof YateChannel.IVR);
 }
 
 //
